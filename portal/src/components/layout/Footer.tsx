e0919f163b77293a042d4a4412afb2ca5d295e10
@RBuldybayev <rbuldybayev@nurbank.kz>
2025-12-23 18:06:18 +0500

    UI: footer height

--- portal/src/components/layout/Footer.tsx
+++ portal/src/components/layout/Footer.tsx
@@ -14,7 +14,7 @@ export const Footer = () => {
 
   return (
     <footer className="w-full flex justify-center">
-      <div className="container mx-auto bg-white rounded-[10px] opacity-100 shadow-[0_2px_8px_rgba(0,0,0,0.1)] py-6 px-4">
+      <div className="container mx-auto bg-white rounded-[10px] opacity-100 shadow-[0_2px_8px_rgba(0,0,0,0.1)] py-2 px-6">
         <div className="flex flex-col md:flex-row items-center justify-between gap-6">
           <div className="flex items-center gap-3">
             <img src={logo} alt="D-TIDE-KG" className="w-[5vw] min-w-[70px] h-auto" />
