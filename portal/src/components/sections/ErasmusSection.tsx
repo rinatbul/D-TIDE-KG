84ae6e345c7cba457420f4cc3e1e49d12804d883
@RBuldybayev <rbuldybayev@nurbank.kz>
2026-01-05 10:22:43 +0500

    UI: ErasmusSection text color

--- portal/src/components/sections/ErasmusSection.tsx
+++ portal/src/components/sections/ErasmusSection.tsx
@@ -18,13 +18,13 @@ export const ErasmusSection = () => {
             <div className="shrink-0">
               <img src={euroFlag} alt="EU Flag" className="w-[20vw] min-w-[200px] h-auto" />
             </div>
-            <h2 className="font-onest font-bold text-[40px] leading-[135%] text-black">
+            <h2 className="font-onest font-bold text-[40px] leading-[135%] text-white">
               Co-funded by the<br />
               Erasmus+ Programme of<br />
               the European Union
             </h2>
           </div>
-          <p className="font-onest font-normal text-lg leading-none text-center text-black max-w-6xl mx-auto">
+          <p className="font-onest font-normal text-lg leading-none text-center text-white max-w-6xl mx-auto">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta dictum magna quis lobortis. Nunc elementum metus quis leo vestibulum porttitor. Cras vel hendrerit diam. Sed et lorem lacinia, aliquam lacus vitae, porta risus. Vestibulum ut velit sed nulla interdum placerat. Etiam turpis turpis, mollis vitae tortor sed, gravida faucibus sem. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras at malesuda tortor, vel condimentum ligula. Etiam dapibus eros nibh, semper tempor odio fringilla quis. Praesent pulvinar quam ut leo vulputate pulvinar vel eget neque. Sed ultrices a nisi id interdum. Maecenas pharetra ante sit amet quam varius, sit amet aliquet nisl ultricies. Suspendisse et efficitur dui, rhoncus malesuada erat. Sed a libero sit amet felis molestie tincidunt nec ac ante. Etiam in urna enim. Suspendisse posuere nisl vel sapien convallis, et sollicitudin eros tincidunt.
           </p>
         </div>
