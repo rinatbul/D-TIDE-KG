2fc03b52c2e245dddca5544dd4edff4e9ef61321
@RBuldybayev <rbuldybayev@nurbank.kz>
2026-01-09 12:06:03 +0500

    UI:Home page sections  paddings

--- portal/src/components/sections/AboutSection.tsx
+++ portal/src/components/sections/AboutSection.tsx
@@ -39,11 +39,11 @@ export const AboutSection = () => {
 
   return (
     <section className="mb-21 bg-[#F0F0F0] pt-21 pb-15">
-      <div className="container mx-auto px-24">
+      <div className="container mx-auto px-4">
         <h2 className="font-onest font-bold text-[36px] leading-none uppercase text-center text-[#0052B4] mb-12">О ПРОЕКТЕ</h2>
         <div className="flex flex-col gap-16 justify-center">
           <div className="flex flex-row gap-8 items-stretch">
-            <div className="bg-[#F0F0F0] rounded-lg p-8 flex-1">
+            <div className="bg-[#F0F0F0] rounded-lg  flex-1">
               <div className="flex items-center gap-4 mb-6">
                 <img src={projectGoalsIcon} alt="Цели проекта" style={{ width: '37px', height: '37px' }} />
                 <h3 className="font-onest font-bold text-xl leading-none text-black">Цель проекта</h3>
--- portal/src/components/sections/NewsSection.tsx
+++ portal/src/components/sections/NewsSection.tsx
@@ -17,7 +17,7 @@ export const NewsSection = () => {
 
   return (
     <section className="mb-21 bg-white">
-      <div className="container mx-auto px-24">
+      <div className="container mx-auto px-4">
         <h2 className="font-onest font-bold text-[36px] leading-none uppercase text-center text-[#0052B4] mb-12">НОВОСТИ</h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
           {mockNews.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((news) => (
